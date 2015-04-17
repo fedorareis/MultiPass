d51b7ef8787a81e2093856e65b4a86f5829289b6
fedorareis <fedorareis@gmail.com>
2015-04-16 21:57:32 -0700

    More function testing
    Finished testing the individual crypto functions.

--- keyGeneration.js
+++ keyGeneration.js
@@ -8,23 +8,22 @@ var publicKey = key.pub.get()
 var serialPubKey = sjcl.codec.base64.fromBits(publicKey.x.concat(publicKey.y))
 var serialPrivateKey = sjcl.codec.base64.fromBits(privateKey)
 
-var pub = new sjcl.ecc.elGamal.publicKey(
-    sjcl.ecc.curves.c256, 
-    sjcl.codec.base64.toBits(serialPubKey)
-)
+var pub = new sjcl.ecc.elGamal.publicKey(sjcl.ecc.curves.c256, sjcl.codec.base64.toBits(serialPubKey))
 
-var sec = new sjcl.ecc.elGamal.secretKey(
-    sjcl.ecc.curves.c256,
-    sjcl.ecc.curves.c256.field.fromBits(sjcl.codec.base64.toBits(serialPrivateKey))
-)
+var sec = new sjcl.ecc.elGamal.secretKey(sjcl.ecc.curves.c256, sjcl.ecc.curves.c256.field.fromBits(sjcl.codec.base64.toBits(serialPrivateKey)))
 
-var ciphertext = sjcl.encrypt(pub, "Hello World")
+var ciphertext = sjcl.encrypt(pub, "Hello World", {mode: "gcm"})
 var decrypt = sjcl.decrypt(sec, ciphertext)
 
-var masterKey = sjcl.misc.pbkdf2(password, )
+//var masterKey = sjcl.misc.cachedPbkdf2(password, 5000)
+var ciphertextSym = sjcl.encrypt(password, "Hello World", {mode: "gcm"})
+var decryptSym = sjcl.decrypt(password, ciphertextSym)
 
+console.log("Password & Key:")
 console.log(password)
+//console.log(masterKey)
 
+console.log("\nRandom & Key Pair:")
 console.log(random)
 console.log(key)
 console.log(privateKey)
@@ -37,3 +36,5 @@ console.log(serialPrivateKey)
 console.log("\nCrypto Test:")
 console.log(ciphertext)
 console.log(decrypt)
+console.log(ciphertextSym)
+console.log(decryptSym)
