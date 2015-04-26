bf3686b1367d62288693c2baf504e86ae7791e69
fedorareis <fedorareis@gmail.com>
2015-04-25 22:18:25 -0700

    Implemented Crypto
    Implemented all of the crypto except Group Key generation. Did basic
    testing of crypto functions, they appear to be working but I need to
    ask Zach how I should be testing it.

--- test.js
+++ test.js
@@ -1,7 +1,59 @@
-//var sjcl = require('./sjcl.js')
+var pair = generateKeyPair()
+var pair2 = generateKeyPair()
+var gKey = secureGKey(pair[1], "bob")
+var cyphertext = testEcrypt("password", pair[0], gKey)
+var decrypted = testDcrypt("password", pair[0], gKey, cyphertext)
+console.log(decrypted)
 
-var ciphertext = sjcl.encrypt("password", "Hello World!")
-var plaintext = sjcl.decrypt("password", ciphertext)
+var gKey2 = shareGKey(getPKey("password", pair[0]), pair2[1], gKey)
+console.log(getGKey(getPKey("password", pair2[0]), gKey2))
 
-console.log(ciphertext)
-console.log(plaintext)
+
+function test(){
+	var password = "password"
+
+	var random = sjcl.random.randomWords(5, 6)
+
+	var key = sjcl.ecc.elGamal.generateKeys(256)
+	var privateKey = key.sec.get()
+	var publicKey = key.pub.get()
+	var serialPubKey = sjcl.codec.base64.fromBits(publicKey.x.concat(publicKey.y))
+	var serialPrivateKey = sjcl.codec.base64.fromBits(privateKey)
+
+	//Recreates the public key from the bitstring after converting it to a bitstring from base64
+	var pub = new sjcl.ecc.elGamal.publicKey(sjcl.ecc.curves.c256, sjcl.codec.base64.toBits(serialPubKey))
+
+	//Recreates the private key from the bitstring after converting it to a bitstring from base64
+	var sec = new sjcl.ecc.elGamal.secretKey(sjcl.ecc.curves.c256, sjcl.ecc.curves.c256.field.fromBits(sjcl.codec.base64.toBits(serialPrivateKey)))
+
+	var ciphertext = sjcl.encrypt(pub, "Hello World", {mode: "gcm"})
+	var decrypt = sjcl.decrypt(sec, ciphertext)
+
+	//var masterKey = sjcl.misc.cachedPbkdf2(password, 5000)
+	var ciphertextSym = sjcl.encrypt(password, "Hello World", {mode: "gcm"})
+	var decryptSym = sjcl.decrypt(password, ciphertextSym)
+
+	console.log("Password & Key:")
+	console.log(password)
+	//console.log(masterKey)
+
+	console.log("\nRandom & Key Pair:")
+	//console.log(random)
+	//console.log(key)
+	console.log(privateKey)
+	console.log(publicKey)
+
+	console.log("\nRecreated keys:")
+	console.log(sec)
+	console.log(pub)
+
+	console.log("\nSerialized Keys:")
+	console.log(serialPubKey)
+	console.log(serialPrivateKey)
+
+	console.log("\nCrypto Test:")
+	console.log(ciphertext)
+	console.log(decrypt)
+	console.log(ciphertextSym)
+	console.log(decryptSym)
+}
