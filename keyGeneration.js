4a0c8e372c18bfa2824b3e9293b0d1f6a35a693c
fedorareis <fedorareis@gmail.com>
2015-04-25 22:19:46 -0700

    Implemented Crypto
    Implemented all of the crypto except Group Key generation. Did basic
    testing of crypto functions, they appear to be working but I need to
    ask Zach how I should be testing it.

---
+++ keyGeneration.js
@@ -0,0 +1,62 @@
+function generateKeyPair(){
+	//Generates keys
+	var key = sjcl.ecc.elGamal.generateKeys(256)
+	var privateKey = key.sec.get()
+	var publicKey = key.pub.get()
+
+	// Converts keys to Base64 for storage
+	var serialPubKey = sjcl.codec.base64.fromBits(publicKey.x.concat(publicKey.y))
+	var serialPrivateKey = sjcl.codec.base64.fromBits(privateKey)
+	privateKey = securePKey("password", serialPrivateKey)
+
+	return [privateKey, serialPubKey]
+}
+
+function securePKey(password, pKey){
+	return sjcl.encrypt(password, pKey, {mode: "gcm"})
+}
+
+function secureGKey(pubKey, gKey){
+	var pub = new sjcl.ecc.elGamal.publicKey(sjcl.ecc.curves.c256, sjcl.codec.base64.toBits(pubKey))
+	return sjcl.encrypt(pub, gKey, {mode: "gcm"})
+}
+
+function securePass(gKey, pass){
+	return sjcl.encrypt(gKey, pass, {mode: "gcm"})
+}
+
+function getPKey(password, cyphertext){
+	return sjcl.decrypt(password, cyphertext)
+}
+
+function getGKey(pKey, cyphertext){
+	var sec = new sjcl.ecc.elGamal.secretKey(sjcl.ecc.curves.c256, sjcl.ecc.curves.c256.field.fromBits(sjcl.codec.base64.toBits(pKey)))
+	return sjcl.decrypt(sec, cyphertext)
+}
+
+function getPass(gKey, cyphertext){
+	return sjcl.decrypt(gKey, cyphertext)
+}
+
+function testEcrypt(pass, pKey, gKey){
+	var pkey = getPKey(pass, pKey)
+	var dcGkey = getGKey(pkey, gKey)
+	return securePass(dcGkey, "This is Awkward")
+}
+
+function testDcrypt(pass, pKey, gKey, cyphertext){
+	var pkey = getPKey(pass, pKey)
+	var dcGkey = getGKey(pkey, gKey)
+	return getPass(dcGkey, cyphertext)
+}
+
+function shareGKey(pKey, pubKey, gKey){
+	var dcGKey = getGKey(pKey, gKey)
+	return secureGKey(pubKey, dcGKey)
+}
+
+function generateGKey(){
+	//TODO
+	console.log("GET THIS DONE!!!!!! (generate group key)")
+}
+
