37f76b70fc51604c55980172db0aa0c6df964dfd
fedorareis <fedorareis@gmail.com>
2018-11-18 00:48:41 -0800

    Add documentation

--- static/keyGeneration.js
+++ static/keyGeneration.js
@@ -1,84 +1,179 @@
-function generateKeyPair(password){
-	//Generates keys
-	var key = sjcl.ecc.elGamal.generateKeys(256)
-	var privateKey = key.sec.get()
-	var publicKey = key.pub.get()
+/**
+ * Generates a key pair and encrypts the private key using the PBKDF results
+ *
+ * @param {string} password
+ *
+ * @return {array} An array consisting of the encrypted private key
+ *                 the base64 encoded public key
+ *                 and the salt used by PBKDF2
+ */
+function generateKeyPair(password) {
+  // Generates keys
+  const key = sjcl.ecc.elGamal.generateKeys(256);
+  let privateKey = key.sec.get();
+  const publicKey = key.pub.get();
 
-	// Converts keys to Base64 for storage
-	var serialPubKey = sjcl.codec.base64.fromBits(publicKey.x.concat(publicKey.y))
-	var serialPrivateKey = sjcl.codec.base64.fromBits(privateKey)
-	var rand = sjcl.random.randomWords(3, 10);
-	var PBK = sjcl.misc.pbkdf2(password, rand, 5000)
-	privateKey = securePKey(PBK, serialPrivateKey)
+  // Converts keys to Base64 for storage
+  const serialPubKey = sjcl.codec.base64.fromBits(publicKey.x.concat(publicKey.y));
+  const serialPrivateKey = sjcl.codec.base64.fromBits(privateKey);
+  const rand = sjcl.random.randomWords(3, 10);
+  const PBK = sjcl.misc.pbkdf2(password, rand, 5000);
 
-	return [privateKey, serialPubKey, rand]
+  // Encrypts the private key for storage
+  privateKey = securePKey(PBK, serialPrivateKey);
+
+  return [privateKey, serialPubKey, rand];
 }
 
-function securePKey(password, pKey){
-	return sjcl.encrypt(password, pKey, {mode: "gcm"})
+/**
+ * Encrypts the private key for storage
+ *
+ * @param {bitArray} password A PBKDF2 bitArray
+ * @param {base64} pKey THe private key to encrypt
+ *
+ * @return {string} The encrypted private key
+ */
+function securePKey(password, pKey) {
+  return sjcl.encrypt(password, pKey, {mode: 'gcm'});
 }
 
-function secureGKey(pubKey, gKey){
-	var pub = new sjcl.ecc.elGamal.publicKey(sjcl.ecc.curves.c256, sjcl.codec.base64.toBits(pubKey))
-	return sjcl.encrypt(pub, sjcl.codec.base64.fromBits(gKey), {mode: "gcm"})
+/**
+ * Encrypts the group key using a public key for storage
+ *
+ * @param {base64} pubKey The public key to use to encrypt the group key
+ * @param {bitArray} gKey The group key to encrypt
+ *
+ * @return {string}
+ */
+function secureGKey(pubKey, gKey) {
+  const pub = new sjcl.ecc.elGamal.publicKey(sjcl.ecc.curves.c256, sjcl.codec.base64.toBits(pubKey));
+  return sjcl.encrypt(pub, sjcl.codec.base64.fromBits(gKey), {mode: 'gcm'});
 }
 
-function securePass(gKey, pass){
-	return sjcl.encrypt(gKey, pass, {mode: "gcm"})
+/**
+ * Encrypts a password using the group key for the group the password belongs to
+ *
+ * @param {bitArray} gKey The group key to use to encrypt the password
+ * @param {string} pass The password to encrypt
+ *
+ * @return {string} The encrypted password
+ */
+function securePass(gKey, pass) {
+  return sjcl.encrypt(gKey, pass, {mode: 'gcm'});
 }
 
-function getPKey(password, cyphertext){
-	return sjcl.decrypt(password, cyphertext)
+/**
+ * Decrypts the private key using the PBKDF2 bitArray
+ *
+ * @param {bitArray} password The PBKDF2 bitArray to use to decrypt the private key
+ * @param {string} cyphertext THe private key to decrypt
+ *
+ * @return {string} The decrypted private key
+ */
+function getPKey(password, cyphertext) {
+  return sjcl.decrypt(password, cyphertext);
 }
 
-function getGKey(pKey, cyphertext){
-	var sec = new sjcl.ecc.elGamal.secretKey(sjcl.ecc.curves.c256, sjcl.ecc.curves.c256.field.fromBits(sjcl.codec.base64.toBits(pKey)))
-	return sjcl.codec.base64.toBits(sjcl.decrypt(sec, cyphertext))
+/**
+ * Decrypts a group key and returns it as a bit array
+ *
+ * @param {base64} pKey
+ * @param {string} cyphertext
+ *
+ * @return {bitArray} The group key
+ */
+function getGKey(pKey, cyphertext) {
+  const sec = new sjcl.ecc.elGamal.secretKey(sjcl.ecc.curves.c256, sjcl.ecc.curves.c256.field.fromBits(sjcl.codec.base64.toBits(pKey)));
+  return sjcl.codec.base64.toBits(sjcl.decrypt(sec, cyphertext));
 }
 
-function getPass(gKey, cyphertext){
-	return sjcl.decrypt(gKey, cyphertext)
+/**
+ * Decrypts the password using the group key for the group the password is in
+ *
+ * @param {bitArray} gKey The group key
+ * @param {string} cyphertext The cyphertext to decrypt
+ *
+ * @return {string} The decrypted password
+ */
+function getPass(gKey, cyphertext) {
+  return sjcl.decrypt(gKey, cyphertext);
 }
 
-function testEcrypt(pass, pKey, gKey){
-	var pkey = getPKey(pass, pKey)
-	var dcGkey = getGKey(pkey, gKey)
-	return securePass(dcGkey, "This is Awkward")
+/**
+ * Encrypts the group key with another users public key
+ *
+ * @param {base64} pKey The private key to decrypt the group key
+ * @param {base64} pubKey The public key to encrypt the group key with
+ * @param {bitArray} gKey The group key to share
+ *
+ * @return {string} The encrypted group key
+ */
+function shareGKey(pKey, pubKey, gKey) {
+  const dcGKey = getGKey(pKey, gKey);
+  return secureGKey(pubKey, dcGKey);
 }
 
-function testDcrypt(pass, pKey, gKey, cyphertext){
-	var pkey = getPKey(pass, pKey)
-	var dcGkey = getGKey(pkey, gKey)
-	return getPass(dcGkey, cyphertext)
+/**
+ * Generates a new group key and encrypts it
+ *
+ * @param {base64} pubKey The public key to use to encrypt the group key
+ *
+ * @return {string} The encrypted group key
+ */
+function generateGKey(pubKey) {
+  const rand = sjcl.random.randomWords(8, 10);
+  return secureGKey(pubKey, rand);
 }
 
-function shareGKey(pKey, pubKey, gKey){
-	var dcGKey = getGKey(pKey, gKey)
-	return secureGKey(pubKey, dcGKey)
+
+/**
+ *
+ * @param {*} pass
+ * @param {*} pKey
+ * @param {*} gKey
+ */
+function testEcrypt(pass, pKey, gKey) {
+  const pkey = getPKey(pass, pKey);
+  const dcGkey = getGKey(pkey, gKey);
+  return securePass(dcGkey, 'This is Awkward');
 }
 
-function generateGKey(pubKey){
-	var rand = sjcl.random.randomWords(8, 10)
-	return secureGKey(pubKey, rand)
+/**
+ *
+ * @param {*} pass
+ * @param {*} pKey
+ * @param {*} gKey
+ * @param {*} cyphertext
+ */
+function testDcrypt(pass, pKey, gKey, cyphertext) {
+  const pkey = getPKey(pass, pKey);
+  const dcGkey = getGKey(pkey, gKey);
+  return getPass(dcGkey, cyphertext);
 }
 
-function testPassEcrypt(){
-	rand = generateGKey()
-	cypher = securePass(rand, "password")
-	console.log(cypher)
-	plain = getPass(rand, cypher)
-	console.log(plain)
+/**
+ *
+ */
+function testPassEcrypt() {
+  rand = generateGKey();
+  cypher = securePass(rand, 'password');
+  console.log(cypher);
+  plain = getPass(rand, cypher);
+  console.log(plain);
 }
 
-function testGEcrypt(){
-	var keys = generateKeyPair("bob")
-	var otherKeys = generateKeyPair("password")
-	var GKey = generateGKey(keys[1])
-	var cypher = secureGKey(keys[1], GKey)
-	data = {};
-	data["keys"] = keys
-	data["otherKeys"] = otherKeys
-	data["GKey"] = GKey
-	data["cypher"] = cypher
-	return data
+/**
+ *
+ */
+function testGEcrypt() {
+  const keys = generateKeyPair('bob');
+  const otherKeys = generateKeyPair('password');
+  const GKey = generateGKey(keys[1]);
+  const cypher = secureGKey(keys[1], GKey);
+  data = {};
+  data['keys'] = keys;
+  data['otherKeys'] = otherKeys;
+  data['GKey'] = GKey;
+  data['cypher'] = cypher;
+  return data;
 }
